4c28edc7ce644dcf2b146c5667c6b9a874d54f31
Niek Peters <n.s.peters05@gmail.com>
2026-09-12 00:13:05 +0200

    Added Electron Wayland hint

--- homeManagerModules/desktop-environment.nix
+++ homeManagerModules/desktop-environment.nix
@@ -1,4 +1,7 @@
 { inputs, ... }: {
   programs.kitty.enable = true;
   wayland.windowManager.hyprland.enable = true;
+
+  # Hint Electron apps to use Wayland
+  home.sessionVariables.NIXOS_OZONE_WL = "1";
 }
