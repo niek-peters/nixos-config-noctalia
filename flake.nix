a4e21022467758fd672243124a375263feaa7e88
Niek Peters <n.s.peters05@gmail.com>
2026-09-11 23:06:52 +0200

    Added home-manager arg

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     };
   };
 
-  outputs = inputs@{ nixpkgs, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
