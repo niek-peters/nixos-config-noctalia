de7c13c5196f2f8f7d530241bda52189f910209e
Niek Peters <n.s.peters05@gmail.com>
2026-09-12 00:39:49 +0200

    Added Noctalia

--- flake.nix
+++ flake.nix
@@ -11,6 +11,10 @@
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     noctalia-greeter = {
       url = "github:noctalia-dev/noctalia-greeter";
       inputs.nixpkgs.follows = "nixpkgs";
--- homeManagerModules/desktop-environment.nix
+++ homeManagerModules/desktop-environment.nix
@@ -1,7 +1,25 @@
 { inputs, ... }: {
+  imports = [
+    inputs.noctalia.homeModules.default
+  ];  
+
   programs.kitty.enable = true;
   wayland.windowManager.hyprland.enable = true;
 
   # Hint Electron apps to use Wayland
   home.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  programs.noctalia = {
+    enable = true;
+    settings = {
+      theme = {
+        mode = "dark";
+        source = "wallpaper";
+      };
+      wallpaper = {
+        enabled = true;
+        default.path = "~/Pictures/Wallpapers/wallhaven-pkw6y3.jpg";
+      };
+    };
+  };
 }
