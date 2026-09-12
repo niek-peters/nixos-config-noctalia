6383571ca42f0db883e6e7845aac47dc85858ca8
Niek Peters <n.s.peters05@gmail.com>
2026-09-12 13:17:40 +0200

    Added a bunch more Hyprland config

--- homeManagerModules/desktop-environment.nix
+++ homeManagerModules/desktop-environment.nix
@@ -6,6 +6,8 @@ let
       (lib.generators.mkLuaInline "hl.dsp.${cmd}")
     ];
   };
+  mkBindExec = key: cmd: (mkBind key ''exec_cmd("${cmd}")'');
+  mkBindIPC = key: cmd: (mkBindExec key "noctalia msg ${cmd}");
   mkExec =
     cmd:
     lib.generators.mkLuaInline ''
@@ -18,6 +20,12 @@ let
   #    ${body}
   #  end
   #'';
+  mkWorkspace = num: {
+    workspace = num;
+    monitor = "eDP-1";
+    persistent = true;
+  };
+  mkFocusWorkspace = i: mkExec "focus({ workspace = ${i} })";
 in
 {
   imports = [
@@ -69,16 +77,57 @@ in
           #'')
         ];
       };
+
       bind = [
         (mkBind "SUPER + Q" "window.close()")
-        (mkBind "SUPER + T" ''exec_cmd("kitty")'')
-        (mkBind "SUPER + W" ''exec_cmd("helium")'')
-        (mkBind "SUPER + E" ''exec_cmd("thunar")'')
-        (mkBind "SUPER + C" ''exec_cmd("code")'')
-        (mkBind "SUPER + Space" ''exec_cmd("noctalia-launcher")'')
+        (mkBindExec "SUPER + T" "kitty")
+        (mkBindExec "SUPER + W" "helium")
+        (mkBindExec "SUPER + E" "thunar")
+        (mkBindExec "SUPER + C" "code")
+
+        # Recommended Noctalia base keybinds
+        (mkBindIPC "SUPER + Space" "panel-toggle launcher")
+        (mkBindIPC "SUPER + S" "panel-toggle control-center")
+        (mkBindIPC "SUPER + comma" "settings-toggle")
+        (mkBindIPC "ALT + Tab" "window-switcher")
+
+        # Workspace switching
+        (mkFocusWorkspace "SUPER + 1" 1)
+        (mkFocusWorkspace "SUPER + 2" 2)
+        (mkFocusWorkspace "SUPER + 3" 3)
+        (mkFocusWorkspace "SUPER + 4" 4)
+        (mkFocusWorkspace "SUPER + 5" 5)
         #(mkBind "SUPER + T" "kitty")
         #(mkBind "SUPER + Space" "noctalia-launcher")
       ];
+
+      workspace_rule = [
+        (mkWorkspace 1)
+        (mkWorkspace 2)
+        (mkWorkspace 3)
+        (mkWorkspace 4)
+        (mkWorkspace 5)
+      ];
+
+      window_rule = {
+        match.class = "dev.noctalia.Noctalia";
+        float = true;
+        size = [
+          1080
+          920
+        ];
+      };
+
+      layer_rule = {
+        name = "noctalia";
+        match = {
+          namespace = "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd|window-switcher)$";
+        };
+        no_anim = true;
+        ignore_alpha = 0.5;
+        blur = true;
+        blur_popups = true;
+      };
     };
     #extraConfig = ''
     #  hl.exec_cmd("noctalia")
