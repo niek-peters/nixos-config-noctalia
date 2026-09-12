67b07055784cae607c5fb3548add6276d20a7f4a
Niek Peters <n.s.peters05@gmail.com>
2026-09-12 16:27:49 +0200

    Again

--- homeManagerModules/desktop-environment.nix
+++ homeManagerModules/desktop-environment.nix
@@ -75,9 +75,9 @@ in
 
       monitor = {
         output = "eDP-1";
-        mode = "1920x1080";
+        mode = "1920x1080@60.01Hz";
         position = "0x0";
-        scale = 1;
+        # scale = 1;
       };
 
       on = {
