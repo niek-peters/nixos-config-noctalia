331512939b8feff268f8abfdb5c2c5a5a1db37b1
Niek Peters <n.s.peters05@gmail.com>
2026-09-13 17:32:24 +0200

    Got sync to work! Now trying the greetd approach again

--- nixosModules/desktop-environment.nix
+++ nixosModules/desktop-environment.nix
@@ -8,24 +8,24 @@
     withUWSM = true;
   };
 
-  # services.greetd = {
-  #   enable = true;
-  #   settings = {
-  #     # Auto login
-  #     initial_session = {
-  #       command = "uwsm start hyprland-uwsm.desktop";
-  #       user = "niek";
-  #     };
-  #     # This fallback keeps your regular login greeter if you ever log out
-  #     default_session = {
-  #       command = "${pkgs.noctalia-greeter}/bin/noctalia-greeter-session --cmd 'uwsm start hyprland-uwsm.desktop'";
-  #       user = "greeter";
-  #     };
-  #   };
-  # };
+  services.greetd = {
+    enable = true;
+    settings = {
+      # Auto login
+      initial_session = {
+        command = "uwsm start hyprland-uwsm.desktop";
+        user = "niek";
+      };
+      # This fallback keeps your regular login greeter if you ever log out
+      default_session = {
+        command = "${inputs.noctalia-greeter}/bin/noctalia-greeter-session --cmd 'uwsm start hyprland-uwsm.desktop'";
+        user = "greeter";
+      };
+    };
+  };
 
   programs.noctalia-greeter = {
-    enable = true;
+    # enable = true;
     passwordless-sync-users = [ "niek" ];
   };
 
