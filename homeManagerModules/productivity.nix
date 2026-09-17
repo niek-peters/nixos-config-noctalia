d440d38c0616a28c6f92dab189b5c2ac879827ad
Niek Peters <n.s.peters05@gmail.com>
2026-09-17 21:52:25 +0200

    Chose to go with Firefox for DRM streaming instead

--- homeManagerModules/productivity.nix
+++ homeManagerModules/productivity.nix
@@ -9,37 +9,7 @@
     inputs.helium-flake.homeModules.default
   ];
 
-  home.packages = with pkgs; [
-    google-chrome
-  ];
-
   programs.helium.enable = true;
-
-  # Helium copy Widevine DRM from Google Chrome
-  home.activation.heliumWidevineFix = lib.mkAfter ''
-    HELIUM_DIR="$HOME/.config/net.imput.helium"
-    WIDEVINE_DEST="$HELIUM_DIR/WidevineCdm"
-    CHROME_WIDEVINE_DIR="${pkgs.google-chrome}/opt/google/chrome/WidevineCdm"
-
-    if [ -d "$CHROME_WIDEVINE_DIR" ]; then
-      mkdir -p "$WIDEVINE_DEST"
-      
-      if [ -d "$CHROME_WIDEVINE_DIR/_platform_specific/linux_x64" ]; then
-        cp -rf "$CHROME_WIDEVINE_DIR/_platform_specific/linux_x64/"* "$WIDEVINE_DEST/"
-      else
-        cp -rf "$CHROME_WIDEVINE_DIR/"* "$WIDEVINE_DEST/"
-      fi
-      
-      # Extract version from manifest.json to satisfy Chromium's component state tracker
-      if [ -f "$WIDEVINE_DEST/manifest.json" ]; then
-        VERSION=$(grep -o '"version": *"[^"]*"' "$WIDEVINE_DEST/manifest.json" | head -n 1 | cut -d'"' -f4)
-        # Write the component tracking file Helium expects
-        echo "$VERSION" > "$HELIUM_DIR/latest-component-updated-widevine-cdm"
-      fi
-
-      chmod -R +rx "$WIDEVINE_DEST"
-    fi
-  '';
 
   programs.vscode = {
     enable = true;
--- nixosModules/desktop-environment.nix
+++ nixosModules/desktop-environment.nix
@@ -81,6 +81,7 @@
 
   environment.systemPackages = with pkgs; [
     thunar
+    firefox
     #noctalia-greeter
     #foot
     #starship
