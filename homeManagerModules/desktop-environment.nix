8e1fa585986ebd9ef0d9a1012edbd46816e00dac
Niek Peters <n.s.peters05@gmail.com>
2026-09-17 22:21:25 +0200

    Changed split keybind to swapsplit

--- homeManagerModules/desktop-environment.nix
+++ homeManagerModules/desktop-environment.nix
@@ -153,8 +153,8 @@ in
         (mkMouseBind "SUPER + mouse:272" "window.drag()")
         (mkMouseBind "SUPER + mouse:273" "window.resize()")
 
-        # Toggle current split orientation between vertical and horizontal
-        (mkBind "SUPER + J" "layout(\"togglesplit\")")
+        # Swap current split orientation between vertical and horizontal
+        (mkBind "SUPER + J" "layout(\"swapsplit\")")
 
         #(mkBind "SUPER + T" "kitty")
         #(mkBind "SUPER + Space" "noctalia-launcher")
