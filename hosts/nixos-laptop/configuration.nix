7528a011078a7b2d183f6c55516d142e56def07a
niek-peters <n.s.peters05@gmail.com>
2026-09-23 22:14:23 +0200

    Trying another acpi_backlight

--- hosts/nixos-laptop/configuration.nix
+++ hosts/nixos-laptop/configuration.nix
@@ -3,7 +3,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelParams = [ "acpi_backlight=video" ];
+  #boot.kernelParams = [ "acpi_backlight=video" ];
   # # device-specific config here
   # hardware.amdgpu.legacySupport.enable = true;
 
