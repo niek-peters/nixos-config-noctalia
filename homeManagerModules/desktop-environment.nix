bd5825e18622075a05af5d4bfbad989b7a744f4a
niek-peters <n.s.peters05@gmail.com>
2026-09-26 15:03:21 +0200

    Trying xwayland zero scaling

--- homeManagerModules/desktop-environment.nix
+++ homeManagerModules/desktop-environment.nix
@@ -129,6 +129,8 @@ in
             disable_splash_rendering = true;
             disable_hyprland_logo = true;
           };
+
+          xwayland.force_zero_scaling = true;
         };
 
         gesture = {
